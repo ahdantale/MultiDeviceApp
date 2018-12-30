6bde0c1eb9a2c23c8285afb79b6542e841ecea15
Abhishek Dantale <abhishekdantale@Abhisheks-MacBook-Pro.local>
2018-12-30 19:13:06 +0530

    Version 0.00

---
+++ AppLayOut/AppLayOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppLayOut
+//
+//  Created by Abhishek Dantale on 30/12/18.
+//  Copyright © 2018 Abhishek Dantale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
